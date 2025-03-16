47657873f4d3cf83430c23a61a28f2d1fbf8c396
adibmajdi <adibmajdi@gmail.com>
2025-03-16 10:06:44 +0700

    Menambahkan kembali kode kalkulator

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, sum;
+
+    cout << "Masukkan angka pertama: ";
+    cin >> num1;
+    cout << "Masukkan angka kedua: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "Hasil penjumlahan: " << sum << endl;
+    
+    return 0;
+}
